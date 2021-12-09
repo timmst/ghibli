9b7824ebe99947130778dd3aba21b76d4849dfb5
timmst <stadetimm@gmail.com>
2021-12-09 17:45:29 +0100

    Export style  object for sx prop

---
+++ client/src/styles/styles.js
@@ -0,0 +1,10 @@
+export const cardStyle = {
+  backgroundColor: "#fff",
+  boxShadow: "2px 4px 25px rgba(0, 0, 0, .1)",
+  borderRadius: "12px",
+  transition: "all .2s linear",
+  "&:hover": {
+    transform: "translate3D(0, -2px, 0)",
+    boxShadow: "2px 8px 45px rgba(0, 0, 0, .15)",
+  },
+};
