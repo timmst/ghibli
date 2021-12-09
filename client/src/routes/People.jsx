9569dd1b0a2dfbecd313e2b582eade974a131c9d
timmst <stadetimm@gmail.com>
2021-12-09 17:46:01 +0100

    Display people data in table instead of grid

--- client/src/routes/People.jsx
+++ client/src/routes/People.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-import { Grid, Card, CardContent, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
 import { useFetch } from "../utils/useFetch";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TablePagination,
+  Paper,
+} from "@mui/material";
 
 const People = () => {
   const [error, setError] = useState(null);
@@ -10,6 +19,40 @@ const People = () => {
   const { data, err, loading } = useFetch(
     "https://ghibliapi.herokuapp.com/films"
   );
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const columns = [
+    { id: "name", label: "Name", minWidth: 170 },
+    { id: "age", label: "Age", minWidth: 50, align: "right" },
+    {
+      id: "gender",
+      label: "Gender",
+      minWidth: 170,
+      align: "right",
+    },
+    {
+      id: "hair_color",
+      label: "Hair color",
+      minWidth: 170,
+      align: "right",
+    },
+    {
+      id: "eye_color",
+      label: "Eye color",
+      minWidth: 170,
+      align: "right",
+    },
+  ];
 
   // console.log({ data, err, loading });
   //https://ghibliapi.herokuapp.com/films
@@ -37,35 +80,111 @@ const People = () => {
   } else {
     return (
       <React.Fragment>
-        <Grid container spacing={2}>
-          {people.map((person) => (
-            <Grid item key={person.id} xs={8} sm={6} md={3}>
-              <Card variant="outlined">
-                <CardContent>
-                  <Typography
-                    sx={{ fontSize: 14 }}
-                    color="text.secondary"
-                    gutterBottom
-                  >
-                    {person.original_title}
-                  </Typography>
-                  <Typography variant="h5" component="div">
+        <Paper sx={{ width: "100%", overflow: "hidden" }}>
+          <TableContainer sx={{ maxHeight: 540 }}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell
+                      sx={{ fontWeight: "bold" }}
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {people
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => {
+                    return (
+                      <TableRow
+                        hover
+                        role="checkbox"
+                        tabIndex={-1}
+                        key={row.code}
+                      >
+                        {columns.map((column) => {
+                          const value = row[column.id];
+                          return (
+                            <TableCell key={column.id} align={column.align}>
+                              {column.format && typeof value === "number"
+                                ? column.format(value)
+                                : value}
+                            </TableCell>
+                          );
+                        })}
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={people.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+        {/* <TableContainer component={Paper}>
+          <Table
+            sx={{
+              minWidth: 650,
+              borderCollapse: "collapse",
+              margin: "25px 0",
+              fontSize: "0.9em",
+              fontFamily: "sans-serif",
+              boxShadow: "0 0 20px rgba(0, 0, 0, 0.15)",
+            }}
+            aria-label="simple table"
+          >
+            <TableHead
+              sx={{
+                backgroundColor: "#009879",
+              }}
+            >
+              <TableRow>
+                <TableCell sx={{ color: "#ffffff" }}>Name</TableCell>
+                <TableCell align="right" sx={{ color: "#ffffff" }}>
+                  Gender
+                </TableCell>
+                <TableCell align="right" sx={{ color: "#ffffff" }}>
+                  Age
+                </TableCell>
+                <TableCell align="right" sx={{ color: "#ffffff" }}>
+                  Eye color
+                </TableCell>
+                <TableCell align="right" sx={{ color: "#ffffff" }}>
+                  Hair color
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {people.map((person) => (
+                <TableRow
+                  key={person.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
                     {person.name}
-                  </Typography>
-                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    {person.gender} Species {person.species}
-                  </Typography>
-                  <Typography sx={{ mb: 1.5 }} variant="body2">
-                    Appears in {person.films}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {person.eye_color} {"eyes"} {"•"} {person.hair_color} hair
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+                  </TableCell>
+                  <TableCell align="right">{person.gender}</TableCell>
+                  <TableCell align="right">{person.age}</TableCell>
+                  <TableCell align="right">{person.eye_color}</TableCell>
+                  <TableCell align="right">{person.hair_color}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer> */}
       </React.Fragment>
     );
   }
