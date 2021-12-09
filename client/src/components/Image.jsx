21983c794aeae7e7a1c533bf8eb4e37042f43550
timmst <stadetimm@gmail.com>
2021-12-09 17:40:01 +0100

    Integrate image into background

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -1,9 +1,9 @@
 import { Paper } from "@mui/material";
-import ghibliLogo from "../assets/ghibli-logo.jpg";
+import ghibliLogo from "../assets/logo.png";
 
 const Image = () => {
   return (
-    <Paper sx={{ mb: 2 }}>
+    <Paper sx={{ maxWidth: "100%", margin: "1rem auto", boxShadow: "none" }}>
       <img alt="missing" src={ghibliLogo} width="400px" />
     </Paper>
   );
