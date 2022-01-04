601e030164fe66a18000d21f7383d89b579f5f05
timmst <stadetimm@gmail.com>
2022-01-04 23:20:46 +0100

    Remove vehicles link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -68,26 +68,6 @@ export default function Navbar() {
           >
             Locations
           </Link>
-          <Link
-            component={RouterLink}
-            to="/vehicles"
-            sx={{
-              color: "#FFFFFF",
-              ml: 5,
-              fontSize: 20,
-              fontFamily: "Roboto",
-              textDecoration: "none",
-              boxShadow: "2px 4px 25px rgba(0, 0, 0, .1)",
-              borderRadius: "12px",
-              transition: "all .2s linear",
-              "&:hover": {
-                transform: "translate3D(0, -2px, 0)",
-                boxShadow: "2px 8px 45px rgba(0, 0, 0, .15)",
-              },
-            }}
-          >
-            Vehicles
-          </Link>
         </Toolbar>
       </AppBar>
       <Toolbar />
